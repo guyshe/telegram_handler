d89f2adffe7a50e53c1402642e94ce80523e61ee
Guy Shemesh <theguyshemesh@gmail.com>
2021-03-13 23:06:31 +0200

    added url

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='telegram-handler',
-    version='1.0',
+    version='1.1',
     description='async telegram handler',
     author='Guyshe',
+    url='https://github.com/guyshe/telegram_handler',
     install_requieres=[
         'requests'
     ]
