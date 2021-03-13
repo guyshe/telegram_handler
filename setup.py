bd400d81b28b5f06e89218cd18efec5356fdb48c
Guy Shemesh <theguyshemesh@gmail.com>
2021-03-13 23:10:36 +0200

    Added long description

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from distutils.core import setup
 
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='telegram-handler',
-    version='1.1',
+    version='1.2',
     description='async telegram handler',
     author='Guyshe',
     url='https://github.com/guyshe/telegram_handler',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requieres=[
         'requests'
     ]
