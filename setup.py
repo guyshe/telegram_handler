6ee03ec0c63ddd759810848667a670893827854c
Guy Shemesh <theguyshemesh@gmail.com>
2021-03-13 23:39:32 +0200

    Fixed package

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from distutils.core import setup
 
 from os import path
@@ -8,12 +9,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='telegram-handler',
-    version='1.2',
+    version='1.3',
     description='async telegram handler',
     author='Guyshe',
     url='https://github.com/guyshe/telegram_handler',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
     install_requieres=[
         'requests'
     ]
